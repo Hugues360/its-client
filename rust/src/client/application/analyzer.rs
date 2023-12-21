4dfc5c753ec1408cb3263d4d19fc9f5d66962a93
Nicolas Buffon <nicolas.buffon@orange.com>
2023-12-21 10:00:08 +0100

    rust: analyzer traits
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

--- rust/src/client/application/analyzer.rs
+++ rust/src/client/application/analyzer.rs
@@ -7,5 +7,17 @@
 // Author: Nicolas BUFFON <nicolas.buffon@orange.com> et al.
 // Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
 
-pub mod application;
-pub mod configuration;
+use crate::client::configuration::Configuration;
+
+use crate::transport::mqtt::topic::Topic;
+use crate::transport::packet::Packet;
+
+use std::sync::{Arc, RwLock};
+
+pub trait Analyzer<T: Topic, C> {
+    fn new(configuration: Arc<Configuration>, context: Arc<RwLock<C>>) -> Self
+    where
+        Self: Sized;
+
+    fn analyze(&mut self, packet: Packet<T>) -> Vec<Packet<T>>;
+}
