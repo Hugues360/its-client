3ca42c4b35d9823441d1d25e4c4b13b1b80a8cbb
Nicolas Buffon <nicolas.buffon@orange.com>
2023-12-27 10:22:50 +0100

    rust: Cause and monitor.rs
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

---
+++ rust/src/exchange/cause.rs
@@ -0,0 +1,43 @@
+// Software Name: its-client
+// SPDX-FileCopyrightText: Copyright (c) 2016-2023 Orange
+// SPDX-License-Identifier: MIT License
+//
+// This software is distributed under the MIT license, see LICENSE.txt file for more details.
+//
+// Author: Nicolas BUFFON <nicolas.buffon@orange.com> et al.
+// Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+
+use crate::exchange::message::Message;
+use crate::exchange::Exchange;
+use std::fmt::Formatter;
+
+pub struct Cause {
+    pub m_type: String,
+    pub id: String,
+}
+
+impl std::fmt::Display for Cause {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "/cause_type:{}/cause_id:{}", self.m_type, self.id)
+    }
+}
+
+impl Cause {
+    fn new(m_type: String, id: String) -> Self {
+        Self { m_type, id }
+    }
+
+    pub fn from_exchange(exchange: &Exchange) -> Option<Cause> {
+        match &exchange.message {
+            Message::CAM(message) => Some(Cause::new(
+                exchange.type_field.clone(),
+                format!("{}/{}", message.station_id, message.generation_delta_time),
+            )),
+            Message::CPM(message) => Some(Cause::new(
+                exchange.type_field.clone(),
+                format!("{}/{}", message.station_id, message.generation_delta_time),
+            )),
+            _ => None,
+        }
+    }
+}
